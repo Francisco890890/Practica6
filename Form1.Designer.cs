4b7caa9f4ec449deb9ebf4460e1b095a94f08f74
Francisco890890 <elpanaderoconelpan456@gmail.com>
2023-11-11 09:31:12 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Practica_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(112, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(448, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Este programa verifica si un numero es par o impar";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(79, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(145, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Escribe un numero";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(451, 66);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(133, 214);
+            listBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(42, 168);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(230, 112);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(230, 112);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(162, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Yellow;
+            button1.Location = new Point(300, 168);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 35);
+            button1.TabIndex = 5;
+            button1.Text = "Verificar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox1);
+            Controls.Add(listBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Par o impar";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ListBox listBox1;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
